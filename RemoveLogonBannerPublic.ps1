5d27664e2c6ebb7515d555c504e36ab9e3501847
mkonkolowicz <mkonkolowicz@gmail.com>
2016-09-23 12:15:21 -0400

    Add files via upload

---
+++ RemoveLogonBannerPublic.ps1
@@ -0,0 +1,7 @@
+﻿#Enter the machine name into the array called "machines"
+$machines = "",""
+$key = "Software\Microsoft\Windows\CurrentVersion\Policies\System"
+$value = "Legalnoticecaption"
+Test-RegValue -Key $key -Value $value -ComputerName $machines -PassThru | Remove-RegValue -Force
+
+Read-Host -Prompt "Press Enter to exit"
